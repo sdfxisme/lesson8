45d695f9ae3830f7bb980de132a180d98e5bd1db
sdfxisme <sdfx@yandex.ru>
2020-01-28 01:08:08 +0300

    ок

--- decorator.py
+++ decorator.py
@@ -1,1 +1,19 @@
-print("h")
+import time
+
+def time_elapsed(f):
+    def wrapper(*args):
+       start=time.time()
+       print(f(*args))
+       stop=time.time()
+       print("Заняло {} секунд".format(stop-start))
+    return wrapper
+
+
+
+@time_elapsed
+def f(x):
+    list_mln = []
+    for i in range(1, x+1):
+        list_mln.append(i)
+    return list_mln
+f(1000000)
