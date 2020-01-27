71277ef5b54d41322014cf79ef4bf8e40e1fed20
sdfxisme <sdfx@yandex.ru>
2020-01-28 01:37:48 +0300

    ок

--- decorator.py
+++ decorator.py
@@ -3,17 +3,30 @@ import time
 def time_elapsed(f):
     def wrapper(*args):
        start=time.time()
-       print(f(*args))
+       f(*args)
        stop=time.time()
        print("Заняло {} секунд".format(stop-start))
     return wrapper
-
-
 
 @time_elapsed
 def f(x):
     list_mln = []
     for i in range(1, x+1):
         list_mln.append(i)
+    print(type(list_mln))
     return list_mln
 f(1000000)
+
+@time_elapsed
+def f_gen(x):
+    list_gen = [x for x in range(x+1)]
+    print(type(list_gen))
+    return list_gen
+f_gen(1000000)
+
+@time_elapsed
+def f_gen1(x):
+    list_gen1 = (x for x in range(x+1))
+    print(type(list_gen1), list_gen1)
+    return list_gen1
+f_gen1(1000000)
