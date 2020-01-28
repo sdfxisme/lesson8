7071adc1bd84921ecbea7defc42ff51b06c386d7
sdfxisme <sdfx@yandex.ru>
2020-01-28 11:34:52 +0300

    ок

--- decorator.py
+++ decorator.py
@@ -1,32 +1,41 @@
 import time
+import os
+import psutil
 
-def time_elapsed(f):
+def time_of_function(f):
     def wrapper(*args):
-       start=time.time()
+       start = time.time()
        f(*args)
-       stop=time.time()
-       print("Заняло {} секунд".format(stop-start))
+       stop = time.time()
+       print('Заняло секунд: {}'.format(stop-start))
     return wrapper
 
-@time_elapsed
+def memory_of_function(f):
+    def wrapper(*args):
+       proc = psutil.Process(os.getpid())
+       a = proc.memory_info().rss / 1000
+       f(*args)
+       proc = psutil.Process(os.getpid())
+       b = proc.memory_info().rss / 1000
+       c = b - a
+       print('Заняло памяти (Kб):', c)
+    return wrapper
+
+
+@time_of_function
+@memory_of_function
 def f(x):
     list_mln = []
     for i in range(1, x+1):
         list_mln.append(i)
-    print(type(list_mln))
     return list_mln
 f(1000000)
 
-@time_elapsed
+@time_of_function
+@memory_of_function
 def f_gen(x):
     list_gen = [x for x in range(x+1)]
-    print(type(list_gen))
     return list_gen
 f_gen(1000000)
 
-@time_elapsed
-def f_gen1(x):
-    list_gen1 = (x for x in range(x+1))
-    print(type(list_gen1), list_gen1)
-    return list_gen1
-f_gen1(1000000)
+
